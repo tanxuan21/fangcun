917792b9e509bdcbafe44230ceda227548a835be
communist <1917443804@qq.com>
2025-08-31 18:49:09 +0800

    add finish review page

--- src/renderer/src/views/RemenberCard/FinishReviewPage/FinishReviewPage.tsx
+++ src/renderer/src/views/RemenberCard/FinishReviewPage/FinishReviewPage.tsx
@@ -1,10 +1,17 @@
 import { Button } from 'antd'
+import styles from './styles.module.scss'
+import { IconTail } from '@renderer/components/Icon'
+
 export const FinishReview = () => {
   return (
-    <div>
+    <div className={styles['finish-review-page']}>
       {/* 喝彩icon */}
-      <Button>review other</Button>
-      <Button></Button>
+      <IconTail className={styles['appluse-icon']} IconName="#icon-zanhehecai-01" />
+      <div className={styles['button-group']}>
+        <Button>Review other</Button>
+        <Button>Casual review</Button>
+        <Button>Summarize</Button>
+      </div>
     </div>
   )
 }
